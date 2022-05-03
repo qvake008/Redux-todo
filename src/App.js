a2db859fafab81538edf760bdb4cbec782111693
Oleh_Slushnyi <qvake008@gmail.com>
2022-05-03 19:54:44 +0300

    Сделал дз Redux task 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material';
 import { AddField } from './components/AddField';
 import { Item } from './components/Item';
+import React from 'react';
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, []);
+
+  function reducer (state, action) {
+    if (action.type === 'ADD_TASK') {
+      return [...state,
+        {
+          id: state.length+1 , 
+          text: action.text , 
+          completed : action.completed
+        }
+      ]
+    }
+
+    return state
+  }
+
+  const addTask = ( input , checked ) => {
+    dispatch({
+      type: "ADD_TASK",
+      text: input,
+      completed : checked
+    })
+  }
+
   return (
     <div className="App">
       <Paper className="wrapper">
         <Paper className="header" elevation={0}>
           <h4>Список задач</h4>
         </Paper>
-        <AddField />
+        <AddField onClickAdd={addTask}/>
         <Divider />
         <Tabs value={0}>
           <Tab label="Все" />
@@ -18,7 +43,9 @@ function App() {
         </Tabs>
         <Divider />
         <List>
-          <Item text="Задача №1" />
+          {state.map(({ id , text , completed }) => (
+            <Item key = {id} text = {text} completed = {completed}/>
+          ))}
         </List>
         <Divider />
         <div className="check-buttons">
--- src/components/AddField.jsx
+++ src/components/AddField.jsx
@@ -2,17 +2,29 @@ import { TextField, Button, Checkbox } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import React from 'react';
 
-export const AddField = () => {
+export const AddField = ( {onClickAdd} ) => {
+  const [input , setInput] = React.useState('')
+  const [checked , setCheked] = React.useState(false)
+
+  const addTask = () => {
+    onClickAdd(input,checked)
+    setInput('')
+    setCheked(false)
+  }
+
   return (
     <div className="field">
       <Checkbox
+        checked={checked} 
+        onClick={(e) => { setCheked(!checked)}}
         className="checkbox"
         icon={<RadioButtonUncheckedIcon />}
         checkedIcon={<CheckCircleIcon />}
       />
-      <TextField placeholder="Введите текст задачи..." variant="standard" fullWidth />
-      <Button>
+      <TextField value={input} onChange={(e) => {setInput(e.target.value)}} placeholder="Введите текст задачи..." variant="standard" fullWidth />
+      <Button onClick={addTask}>
         <AddIcon />
       </Button>
     </div>
