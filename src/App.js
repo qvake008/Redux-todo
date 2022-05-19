01d6e0d3557c08734189edfbe36095716b0c2000
Your Name <you@example.com>
2022-05-19 16:39:46 +0300

    Сделал задание №3

--- src/App.js
+++ src/App.js
@@ -4,53 +4,145 @@ import { Item } from './components/Item';
 import React from 'react';
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, [
-    {
-      id: 1,
-      text: "Task 1" , 
-      completed : false
+  const [tab, setTab] = React.useState(0)
+  const [selected , setSelected] = React.useState(false)
+  const [state, dispatch] = React.useReducer(reducer, []);
+  const [content, setContent] = React.useState(state)
+
+  React.useEffect(() => {
+    let count = 0 ;
+    state.forEach(item => {
+      if (item.completed === true) {
+        count +=1
+      }
+    })
+    if (count === state.length){ 
+      setSelected(true)
+    } else {
+      setSelected(false)
     }
-  ]);
+
+    if(tab === 0) {
+      setTab(0)
+      setContent(state)
+    } else if (tab === 1) {
+      setTab(1)
+      setContent(state.filter((item)=> item.completed === false))
+    } else if (tab === 2) {
+      setTab(2)
+      setContent(state.filter((item)=> item.completed === true))
+    }
+
+  },[state])
 
   function reducer (state, action) {
     switch (action.type) {
       case 'ADD_TASK':
         return [...state,
           {
-            id: state[state.length - 1].id + 1,
+            id: state.length !== 0 ? state[state.length - 1].id + 1 : 1,
             text: action.payload.text , 
             completed : action.payload.completed
           }
         ];
       case 'DELETE_TASK':
-        if (window.confirm('Вы точно хотите удалить это задание?')){
-          return state.filter(item => item.id !== action.payload.id && item.text !== action.payload.text)
-        }else {
-          return state
+          return state.filter(item => item.id !== action.payload.id)
+      case 'TOGGLE_TASK':
+          return state.map((item) => {
+            if (item.id === action.payload.id){
+              return {
+                ...item,
+                completed: !item.completed
+              }
+            }
+            return item
+          } )
+      case 'CLEAR_TASK':
+        return []
+      case 'SELECT_ALL':
+        if (selected === true){
+          return state.map(item => {
+            return {
+              ...item,
+              completed : false
+            }
+          })
+        } else {
+          return state.map(item => {
+            return {
+              ...item,
+              completed : true
+            }
+          })
         }
       default:
-        return state
+        return []
     }
   }
 
   const addTask = ( input , checked ) => {
+    if(input === '') {
+      window.alert('Введите текст задачи')
+    } else {
+      dispatch({
+        type: "ADD_TASK",
+        payload: {
+          text : input,
+          completed: checked
+        }
+      })
+    }
+    
+  }
+
+  const removeTask = ( id ) => {
+    if (window.confirm('Вы точно хотите удалить это задание?')){
+      dispatch({
+        type: "DELETE_TASK",
+        payload: {
+          id
+        }
+      })
+    }
+  }
+
+  const toggleTask = (id) => {
     dispatch({
-      type: "ADD_TASK",
+      type: "TOGGLE_TASK",
       payload: {
-        text : input,
-        completed: checked
+        id
       }
     })
   }
 
-  const removeTask = ( id , text ) => {
+  const selectAll = () => {
     dispatch({
-      type: "DELETE_TASK",
-      payload: {
-        id,
-        text
-      }
+      type: "SELECT_ALL"
     })
+  }
+
+  const clear = () => {
+    if (window.confirm('Вы точно хотите удалить все заданиея?')){
+      dispatch({
+        type: "CLEAR_TASK"
+      })
+    }
+    setTab(0)
+  }
+
+  const tabAll = () => {
+    setTab(0)
+    setContent(state)
+  }
+
+  const tabActive = () => {
+    setTab(1)
+    setContent(state.filter((item)=> item.completed === false))
+  }
+
+  const tabCompleted = () => {
+    setTab(2)
+    setContent(state.filter((item)=> item.completed === true))
   }
 
   return (
@@ -61,21 +153,21 @@ function App() {
         </Paper>
         <AddField onClickAdd={addTask}/>
         <Divider />
-        <Tabs value={0}>
-          <Tab label="Все" />
-          <Tab label="Активные" />
-          <Tab label="Завершённые" />
+        <Tabs value={tab}>
+          <Tab onClick={tabAll} label="Все" />
+          <Tab onClick={tabActive} label="Активные" />
+          <Tab onClick={tabCompleted} label="Завершённые" />
         </Tabs>
         <Divider />
         <List>
-          {state.map(({ id , text , completed }) => (
-            <Item key = {id + text} id={id} text = {text} completed = {completed} oClickRemove={removeTask}/>
+          {content.map(({ id , text , completed }) => (
+            <Item key = {id + text} toggleTask = {() => toggleTask(id)} id={id} text = {text} completed = {completed} onClickRemove={() => removeTask(id)}/>
           ))}
         </List>
         <Divider />
         <div className="check-buttons">
-          <Button>Отметить всё</Button>
-          <Button>Очистить</Button>
+          <Button onClick={selectAll}>{selected === true ? 'Снять отметки' : 'Отметить всё'}</Button>
+          <Button onClick={clear}>Очистить</Button>
         </div>
       </Paper>
     </div>
